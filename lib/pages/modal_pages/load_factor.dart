a80ecabeca549c481c89a751a8ae1453731ee037
volard <kirillzhilenkov@gmail.com>
2023-05-20 02:51:18 +0800

    :sparkles: Add tex support

--- lib/pages/modal_pages/load_factor.dart
+++ lib/pages/modal_pages/load_factor.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tex/flutter_tex.dart';
 
-Widget get loadFactorPage =>
-    const Column(
+Widget get loadFactorPage => const Column(
       children: [
         Text("Load factor info"),
+        TeXView(
+          child: TeXViewColumn(children: [
+            TeXViewInkWell(
+              id: "id_0",
+              child: TeXViewColumn(children: [
+                TeXViewDocument(r"""<h2>Flutter \( \rm\\TeX \)</h2>""",
+                    style: TeXViewStyle(textAlign: TeXViewTextAlign.center)),
+                TeXViewContainer(
+                  child: TeXViewImage.network(
+                      'https://raw.githubusercontent.com/shah-xad/flutter_tex/master/example/assets/flutter_tex_banner.png'),
+                  style: TeXViewStyle(
+                    margin: TeXViewMargin.all(10),
+                    borderRadius: TeXViewBorderRadius.all(20),
+                  ),
+                ),
+                TeXViewDocument(r"""<p>                                
+                                 When \(a \ne 0 \), there are two solutions to \(ax^2 + bx + c = 0\) and they are
+                                 $$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$</p>""",
+                    style: TeXViewStyle.fromCSS(
+                        'padding: 15px; color: white; background: green'))
+              ]),
+            )
+          ]),
+          style: TeXViewStyle(
+            elevation: 10,
+            borderRadius: TeXViewBorderRadius.all(25),
+            border: TeXViewBorder.all(TeXViewBorderDecoration(
+                borderColor: Colors.blue,
+                borderStyle: TeXViewBorderStyle.solid,
+                borderWidth: 5)),
+            backgroundColor: Colors.white,
+          ),
+        ),
       ],
     );
